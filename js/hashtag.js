bbced49ba1590b117d46c1dfc21b7c4e904a6fe5
mmosaad90 <119763218+mmosaad90@users.noreply.github.com>
2022-12-10 21:43:37 +0200

    Update hashtag.js

--- js/hashtag.js
+++ js/hashtag.js
@@ -1,1 +1,1 @@
-$(document).ready(function(e,n){var t,o=window.location.origin,r=n.querySelectorAll(".post-body");if(0<r.length)for(t=0;t<r.length;t+=1)r[t].innerHTML=r[t].innerHTML.replace(/#(\S+)/g,'<a class="hashtags" href="'+o+'/search?q=$1">#$1</a>')}(0,this.document));
+hashtags=jQuery.noConflict();hashtags(document).ready(function(e,n){"use strict";var t,o=window.location.origin,r=n.querySelectorAll(".post-body");if(0<r.length)for(t=0;t<r.length;t+=1)r[t].innerHTML=r[t].innerHTML.replace(/#(\S+)/g,'<a class="hashtags" href="'+o+'/search?q=$1">#$1</a>')}(0,this.document));
